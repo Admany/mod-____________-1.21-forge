519e0c8afe9907881e786f0177891ee65fdd3c97
Admany <vapsvaarmandas@gmail.com>
2024-08-18 14:37:33 +0300

    Initial commit

---
+++ src/main/java/net/ZAPP/ThePhantomOnes/ThePhantomOnes.java
@@ -0,0 +1,47 @@
+package net.ZAPP.ThePhantomOnes;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ThePhantomOnes.MODID)
+public class ThePhantomOnes
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "thephantomones";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+
+
+
+    public ThePhantomOnes()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+}
+
+
